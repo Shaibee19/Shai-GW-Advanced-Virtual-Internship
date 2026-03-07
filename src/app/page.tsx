855124de3bd3ffc48a411151c94dd6490bf6c5af
Shaibee19 <shaipattzgray@gmail.com>
2026-03-06 23:53:21 -0500

    added the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../app/components/Navbar";
+import Footer from "../app/components/Footer";
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Footer />
     </>
   );
 }
