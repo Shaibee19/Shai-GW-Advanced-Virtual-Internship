e8bad104f963af6ac50e31f9ddb665e68a68884d
Shaibee19 <shaipattzgray@gmail.com>
2026-03-06 23:23:38 -0500

    doesn't add up from what is here to what is in github

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,20 @@
+import logo from "../assets/logo.png";
+import styles from "../globals.css";
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+      <div className="nav__wrapper">
+        <figure className="nav__img--mask">
+          <img className="nav__img" src={logo} alt="logo" />
+        </figure>
+        <ul className="nav__list--wrapper">
+          <li className="nav__list nav__list--login">Login</li>
+          <li className="nav__list nav__list--mobile">About</li>
+          <li className="nav__list nav__list--mobile">Contact</li>
+          <li className="nav__list nav__list--mobile">Help</li>
+        </ul>
+      </div>
+    </nav>
+    )
+}
